28d9f490766fa23b3f5e5b42179e6bdcca1dea94
Isaac Martinez DAM <cf17isaac.martinez@iesjoandaustria.org>
2018-12-20 19:59:32 +0100

    repte 1 done

--- app/src/main/java/com/example/jcmilena/apptomodify1/ControllerActivity.java
+++ app/src/main/java/com/example/jcmilena/apptomodify1/ControllerActivity.java
@@ -5,9 +5,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ControllerActivity extends AppCompatActivity  {
+public class ControllerActivity extends AppCompatActivity  implements EasyFragment.FragmentWombatListener {
 
-    String fragment_number = "1";
+    int fragment_number = 1;
     int fragment_color = R.color.colorFragment1;
     int fragment_container = R.id.fragment_container1;
     String fragment_tag = "Frag1";
@@ -17,8 +17,11 @@ public class ControllerActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
 
+        /* FALTA ALGO */
 
         FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = EasyFragment.newInstance(fragment_number, fragment_color);
+
         fm.beginTransaction().replace(fragment_container, fragment,fragment_tag).commit();
 
     }
@@ -29,11 +32,31 @@ public class ControllerActivity extends AppCompatActivity  {
             FragmentManager fm = getSupportFragmentManager();
             Fragment fragmentToRemove = fm.findFragmentByTag("Frag1");
             fm.beginTransaction().remove(fragmentToRemove).commit();
+            fragment_number = 2;
+            fragment_color = R.color.colorFragment2;
+            fragment_container = R.id.fragment_container2;
+            fragment_tag = "Frag2";
+            Fragment fragment = EasyFragment.newInstance(fragment_number, fragment_color);
+            fm.beginTransaction().replace(fragment_container, fragment, fragment_tag).remove(fragmentToRemove).commit();
+        } else {
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment fragmentToRemove = fm.findFragmentByTag("Frag2");
+            fm.beginTransaction().remove(fragmentToRemove).commit();
+            fragment_number = 1;
+            fragment_color = R.color.colorFragment1;
+            fragment_container = R.id.fragment_container1;
+            fragment_tag = "Frag1";
+            Fragment fragment = EasyFragment.newInstance(fragment_number, fragment_color);
+            fm.beginTransaction().replace(fragment_container, fragment, fragment_tag).remove(fragmentToRemove).commit();
         }
-        else{
-
-        }
-
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("numero", fragment_number);
+        outState.putInt("color",fragment_color);
+        outState.putInt("container", fragment_container);
+        outState.putString("tag", fragment_tag);
+    }
 }
--- app/src/main/java/com/example/jcmilena/apptomodify1/EasyFragment.java
+++ app/src/main/java/com/example/jcmilena/apptomodify1/EasyFragment.java
@@ -1,40 +1,34 @@
 package com.example.jcmilena.apptomodify1;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 
 public class EasyFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "numero";
-    private static final String ARG_PARAM2 = "color";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1 ="1";
+    private static final String argumentOne = "numero";
+    private static final String argumentTwo = "color";
+
+    private Integer mParam1 = 1;
     private int mParam2 = R.color.colorBlanco;
+    FragmentWombatListener wombatListener;
 
+    public EasyFragment() {}
 
+    public static EasyFragment newInstance(int param1, int param2) {
 
-    public EasyFragment() {
-        // Required empty public constructor
-    }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static EasyFragment newInstance(String param1) {
         EasyFragment fragment = new EasyFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putInt(argumentOne, param1);
+        args.putInt(argumentTwo, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -42,14 +36,15 @@ public class EasyFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam1 = getArguments().getInt(argumentOne);
+            mParam2 = getArguments().getInt(argumentTwo);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_easy, container, false);
 
         TextView textView = (TextView) view.findViewById(R.id.text_fragment);
@@ -60,20 +55,31 @@ public class EasyFragment extends Fragment {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Acció a fer
-                }
-        });
 
+                wombatListener.pulsado(Integer.toString(mParam1));
+            }
+        });
 
         return view;
     }
 
     @Override
     public void onAttach(Context context) {
-        super.onAttach(context);
 
+        super.onAttach(context);
+        wombatListener = (FragmentWombatListener)getActivity();
     }
 
+    @Override public void onDetach() {
+
+        super.onDetach();
+    }
+
+    //Introducimos la interface
+    public interface FragmentWombatListener {
+
+        public void pulsado (String numero);
+    }
 
 
 }
